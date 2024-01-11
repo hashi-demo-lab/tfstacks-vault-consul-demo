913bd80c69e5125bc1f9b11bfb089d85635c90d9
Simon Lynch <simon.lynch@hashicorp.com>
2024-01-11 12:01:03 +1100

    test source

--- aws-eks-fargate/main.tf
+++ aws-eks-fargate/main.tf
@@ -7,7 +7,7 @@ locals {
 
 
 module "eks" {
-  source  = "github.com/terraform-aws-modules/terraform-aws-eks"
+  source  = "git@github.com:terraform-aws-modules/terraform-aws-eks.git?ref=v19.21.0"
 
   cluster_name                   = var.cluster_name
   cluster_version                = var.kubernetes_version   #"1.27" 
