3fb19d20e8a9f36825fa61f42c03ea83170aca4e
Simon Lynch <simon.lynch@hashicorp.com>
2024-01-11 15:22:21 +1100

    update ref

--- aws-eks-fargate/main.tf
+++ aws-eks-fargate/main.tf
@@ -7,7 +7,7 @@ locals {
 
 
 module "eks" {
-  source  = "git::https://github.com/hashi-demo-lab/terraform-aws-eks.git?ref=v19.21.1"
+  source  = "git::https://github.com/hashi-demo-lab/terraform-aws-eks.git?ref=v19.21.2"
 
 
   cluster_name                   = var.cluster_name
