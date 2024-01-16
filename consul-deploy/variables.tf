a33d3aeb9dc0a4ced8e2e209399d701798577ae6
Simon Lynch <simon.lynch@hashicorp.com>
2024-01-16 16:31:30 +1100

    update vars

--- consul-deploy/variables.tf
+++ consul-deploy/variables.tf
@@ -12,7 +12,25 @@ variable "consul_version" {
   description = "Consul version"
   type        = string
 }
+variable "private_endpoint_url" {
+  description = "Private endpoint url"
+  type        = string
+}
 
+variable "bootstrap_token" {
+  description = "ACL bootstrap token"
+  type        = string
+}
+
+variable "gossip_encrypt_key" {
+  description = "Gossip encryption key"
+  type        = string
+}
+
+variable "client_ca_cert" {
+  description = "Client ca certificate"
+  type        = string
+}
 
 variable "replicas" {
   description = "Number of replicas"
