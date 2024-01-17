4adf211cb04489f50492daff3c9667cc2a69182b
Simon Lynch <simon.lynch@hashicorp.com>
2024-01-17 12:03:56 +1100

    add consul security groups

--- hcp-hvn-transit/hvn.tf
+++ hcp-hvn-transit/hvn.tf
@@ -65,3 +65,90 @@ resource "aws_route" "hcp_hvn_route" {
   destination_cidr_block    = var.hvn_cidr
   transit_gateway_id        = module.tgw.ec2_transit_gateway_id
 } 
+
+
+#### Rules for HCP Connectivity
+
+module "sg-consul" {
+  source = "terraform-aws-modules/security-group/aws"
+  version     = "4.9.0"
+
+  name        = "${var.deployment_id}-consul"
+  vpc_id      = var.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      description = "consul-partition-https-tcp"
+      cidr_blocks = var.aws_vpc_cidr
+    },
+    {
+      from_port   = 8300
+      to_port     = 8301
+      protocol    = "tcp"
+      description = "consul-rpc-lan-serf-gosspip-tcp"
+      cidr_blocks = var.aws_vpc_cidr
+    },
+    {
+      from_port   = 8300
+      to_port     = 8301
+      protocol    = "udp"
+      description = "consul-lan-serf-gosspip-udp"
+      cidr_blocks = var.aws_vpc_cidr
+    },
+    {
+      from_port   = 8443
+      to_port     = 8443
+      protocol    = "tcp"
+      description = "consul-mesh-gateways"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 8500
+      to_port     = 8502
+      protocol    = "tcp"
+      description = "consul-http-https-api-tcp"
+      cidr_blocks = var.aws_vpc_cidr
+    },
+    {
+      from_port   = 8600
+      to_port     = 8600
+      protocol    = "tcp"
+      description = "consul-dns-tcp"
+      cidr_blocks = var.aws_vpc_cidr
+    },
+    {
+      from_port   = 8600
+      to_port     = 8600
+      protocol    = "udp"
+      description = "consul-dns-udp"
+      cidr_blocks = var.aws_vpc_cidr
+    },
+    {
+      from_port   = 8080
+      to_port     = 8080
+      protocol    = "tcp"
+      description = "consul-connect-injector-tcp"
+      cidr_blocks = var.aws_vpc_cidr
+    },
+    {
+      from_port   = 20000
+      to_port     = 21255
+      protocol    = "tcp"
+      description = "consul-connect-envoy-tcp"
+      cidr_blocks = var.aws_vpc_cidr
+    }
+  ]
+
+  egress_with_cidr_blocks = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      description = "any-any"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+}
