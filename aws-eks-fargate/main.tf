970f2f4275d82b7e0b4c3bdab7a963892ecf224e
Simon Lynch <simon.lynch@hashicorp.com>
2024-02-18 17:29:51 +1100

    update2 eks faragte module

--- aws-eks-fargate/main.tf
+++ aws-eks-fargate/main.tf
@@ -7,7 +7,8 @@ locals {
 
 
 module "eks" {
-  source  = "git::https://github.com/hashi-demo-lab/terraform-aws-eks.git?ref=v19.21.4"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.2.0"
 
   cluster_name                   = var.cluster_name
   cluster_version                = var.kubernetes_version 
@@ -16,8 +17,7 @@ module "eks" {
   vpc_id     = var.vpc_id
   subnet_ids = var.private_subnets
 
-  manage_aws_auth_configmap = var.manage_aws_auth_configmap
-  aws_auth_roles = var.aws_auth_roles
+  enable_irsa = true
 
   # Fargate profiles use the cluster primary security group so these are not utilized
   create_cluster_security_group = false
@@ -28,8 +28,9 @@ module "eks" {
   fargate_profiles = {
     app_wildcard = {
       selectors = [
-        { namespace = "consul*" },
+        { namespace = "hashibank*" },
         { namespace = "product*" },
+        { namespace = "consul*" },
         { namespace = "frontend*" },
         { namespace = "payments*" }
       ]
@@ -42,5 +43,56 @@ module "eks" {
     }
   }
 
+ 
+
+  enable_cluster_creator_admin_permissions = true
+
+  access_entries = {
+      # One access entry with a policy associated
+      single = {
+        kubernetes_groups = []
+        principal_arn     = var.eks_clusteradmin_arn
+        username          = var.eks_clusteradmin_username
+
+        policy_associations = {
+          single = {
+            policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+            access_scope = {
+              type       = "cluster"
+            }
+          }
+        }
+      }
+    }
+
+
   tags = local.tags
+
+
 }
+
+data "aws_eks_cluster" "upstream" {
+  depends_on = [module.eks]
+  name = var.cluster_name
+
+}
+
+data "aws_eks_cluster_auth" "upstream_auth" {
+  depends_on = [module.eks]
+  name = var.cluster_name
+}
+
+
+resource "aws_eks_identity_provider_config" "oidc_config" {
+  depends_on = [module.eks]
+  cluster_name = var.cluster_name
+
+  oidc {
+    identity_provider_config_name = "tfstack-terraform-cloud"
+    client_id                     = var.tfc_kubernetes_audience
+    issuer_url                    = var.tfc_hostname
+    username_claim                = "sub"
+    groups_claim                  = "terraform_organization_name"
+  }
+}
+
